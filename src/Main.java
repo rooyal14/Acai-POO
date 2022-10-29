ce83b3b6c965cbf0f3826ebc7e10b227ae4eb980
Rodrigo Zwirtes <zwi.rodrigo@gmail.com>
2022-10-29 03:26:29 -0400

    Implementação parcial da classe pedido

--- src/Main.java
+++ src/Main.java
@@ -4,64 +4,70 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Pedido pedidinho = new Pedido("Rodrigo", "6922241350", "123-123-123-12");
+		System.out.println(pedidinho.rua);
+		pedidinho.definirEndereço("tranquedo", "tilapia", "6924");
+		System.out.println(pedidinho.rua);
+		
+		
 		ArrayList<Acai> listaDeAcais = new ArrayList<Acai>();
-		
-		//listaDeAcais.add(new Acai("assai", 69.24, 300, array));
-		//listaDeAcais.add(new Acai("assai3", 69.24, 300, array));
-		
-		//System.out.println(listaDeAcais.get(1).listaDeAcomps.get(0));
-		
-		
+			
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Insira 1 para cadastrar ou insira 2 para fazer pedido");
-		int input = scanner.nextInt();
-		scanner.nextLine();
+		int input = scanner.nextInt();scanner.nextLine();
 		System.out.println(input);
 		
 		if (input == 1) {
+			//CADASTRA
 			System.out.println("Bem vindo ao cadastramento");
-			boolean x = true;
-			while (x) {
-				x = adicionar(listaDeAcais, scanner);
-			}
+			adicionar(listaDeAcais, scanner);
+			
 		} else if(input == 2) {
+			//MOSTRA AÇAÍS
 			System.out.println("Bem vindo ao atendimento");
+			mostrar(listaDeAcais, scanner);
 			
 		} else {
-			System.out.println("DEU ruim");
+			System.out.println("else");
+			
 		}
 				
 		scanner.close();
 		
 	}
 	
-	
-	static boolean adicionar(ArrayList<Acai> lista, Scanner scanner) {
-		System.out.println("Insira o nome do açaí para ser listado ao menu");
-		String nome = scanner.nextLine();
-		System.out.println("Insira o preço do açaí para ser listado ao menu");
-		double preco = scanner.nextDouble();scanner.nextLine();
-		System.out.println("Insira o tamanho em ml do açaí para ser listado ao menu");
-		int tamanho = scanner.nextInt();scanner.nextLine();
-		System.out.println("Insira a quantidade de acompanhamentos do açaí para ser listado ao menu");
-		int qtdAcomp = scanner.nextInt();scanner.nextLine();
-		ArrayList<String> arrayDeAcomp = new ArrayList<String>();
-		for (int i = 0; i < qtdAcomp; i++) {
-			System.out.println("Insira o acompanhamento do açaí a ser listado ao menu");
-			String acompanhamento = scanner.nextLine();
-			arrayDeAcomp.add(acompanhamento);
-		}
-		lista.add(new Acai(nome, preco, tamanho, arrayDeAcomp));
+	static void mostrar(ArrayList<Acai> lista, Scanner scanner) {
 		
-		System.out.println("Digite 1 se deseja adicionar mais um tipo de açaí ou 2 se deseja voltar para o menu principal: ");
-		int continuar = scanner.nextInt();scanner.nextLine();
-		if (continuar == 1) {
-			return true;
-		} else if (continuar == 2) {
-			System.out.println("Voltando para o menu principal");
-			return false;
-		}
-		return true;
 	}
+	
+	static void adicionar(ArrayList<Acai> lista, Scanner scanner) {
+		while (true) {
+			System.out.println("Insira o nome do açaí para ser listado ao menu");
+			String nome = scanner.nextLine();
+			System.out.println("Insira o preço do açaí para ser listado ao menu");
+			double preco = scanner.nextDouble();scanner.nextLine();
+			System.out.println("Insira o tamanho em ml do açaí para ser listado ao menu");
+			int tamanho = scanner.nextInt();scanner.nextLine();
+			System.out.println("Insira a quantidade de acompanhamentos do açaí para ser listado ao menu");
+			int qtdAcomp = scanner.nextInt();scanner.nextLine();
+			ArrayList<String> arrayDeAcomp = new ArrayList<String>();
+			for (int i = 0; i < qtdAcomp; i++) {
+				System.out.println("Insira o acompanhamento do açaí a ser listado ao menu");
+				String acompanhamento = scanner.nextLine();
+				arrayDeAcomp.add(acompanhamento);
+			}
+			lista.add(new Acai(nome, preco, tamanho, arrayDeAcomp));
+			
+			System.out.println("Digite 1 se deseja adicionar mais um tipo de açaí ou 2 se deseja voltar para o menu principal: ");
+			int continuar = scanner.nextInt();scanner.nextLine();
+			if (continuar == 1) {
+				continue;
+			} else if (continuar == 2) {
+				break;
+			}
+		}
+	}
+	
+	
 
 }
---
+++ src/Pedido.java
@@ -0,0 +1,21 @@
+
+public class Pedido {
+	String nome;
+	String numeroDeTelefone;
+	String cpf;
+	String bairro;
+	String rua;
+	String numeroDaCasa;
+	
+	public Pedido(String nome, String numeroDeTelefone, String cpf) {
+		this.nome = nome;
+		this.numeroDeTelefone = numeroDeTelefone;
+		this.cpf = cpf;
+	}
+	public void definirEndereço(String bairro, String rua, String numeroDaCasa) {
+		this.bairro = bairro;
+		this.rua = rua;
+		this.numeroDaCasa = numeroDaCasa;
+	}
+	
+}
